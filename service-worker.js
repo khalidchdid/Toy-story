a468811e298fe832d8f2188d15a09aea00f97e5f
khalidchdid <akhmouchkhalid20@gmail.com>
2025-12-30 02:35:09 +0100

    Add service worker for PWA caching

---
+++ service-worker.js
@@ -0,0 +1,38 @@
+const CACHE = "pong-pwa-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./game.js",
+  "./manifest.webmanifest",
+  "./service-worker.js"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      return cached || fetch(event.request).then((resp) => {
+        // Optional runtime caching
+        const copy = resp.clone();
+        caches.open(CACHE).then(cache => cache.put(event.request, copy));
+        return resp;
+      }).catch(() => caches.match("./index.html"));
+    })
+  );
+});
